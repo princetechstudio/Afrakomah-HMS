9168bdef92a0707e5f18a4b53ea717637d544f60
princetechstudio <princetechstudio@gmail.com>
2026-08-28 16:59:50 +0000

    Fix web app installation prompt

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "afrakomah-hms-v2";
-const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/logo.png"];
+const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/logo.png", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
